6917af3e694458048d62feb7b0fe4d37c731e712
İsmail <ismailiris@gmail.com>
2022-01-25 14:59:22 +0300

    middleware denemeleri

---
+++ Middlewares/HelloMiddleware.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace MiddlewarePractices.Middlewares
+{
+    public class HelloMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public HelloMiddleware(RequestDelegate next)
+        {
+            _next=next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine("Hello middleware");
+            await _next.Invoke(context);
+            Console.WriteLine("Bye middleware");
+        }
+    }
+
+    static public class HelloMiddlewareExtension
+    {
+        public static IApplicationBuilder UseHello(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HelloMiddleware>();
+        }
+    }
+}
